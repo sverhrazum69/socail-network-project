ec91cab77c655ebc670493923bcb675a50dcc72a
Nikita <n.osypa666@gmail.com>
2020-07-07 14:02:21 +0300

    implemented basic layaout

---
+++ frontend/src/components/Avatars/Avatars.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+const AvatarImg = props => {
+    return (
+        <Avatar size={128} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+    )
+}
+
+export default AvatarImg
---
+++ frontend/src/containers/FriendList/FriendList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import fm from './FriendList.module.css'
+import { Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import AvatarImg from '../../components/Avatars/Avatars';
+
+
+class FirendList extends React.Component {
+    render(){
+        return (
+            <div>
+                <div className={fm.wrapper}>
+                    <AvatarImg />
+                    <AvatarImg />
+                    <AvatarImg />
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <Button type="primary" shape="round"  size={256}>
+                    LoadMore
+                </Button>
+            </div>
+        )
+    }
+}
+
+export default FirendList
