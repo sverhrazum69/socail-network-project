829853099f649b47bb0214b67bf33b1eafec42bb
root <n.osypa666@gmail.com>
2020-07-07 15:32:07 +0000

    added docker, created custom user model, changed db to postgresql

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request):
-    return HttpResponse("hello")
+    return HttpResponse("hello docker")
 # Create your views here.
--- backend/prj/asgi.py
+++ backend/prj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for settingsRoot project.
+ASGI config for prj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settingsRoot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prj.settings')
 
 application = get_asgi_application()
