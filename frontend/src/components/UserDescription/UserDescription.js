edba7a1e3e1cb5ab0d4063f3f7cce6c963573d43
root <n.osypa666@gmail.com>
2020-07-10 14:26:11 +0000

    rendered avatar

--- frontend/src/components/UserDescription/UserDescription.js
+++ frontend/src/components/UserDescription/UserDescription.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Descriptions } from 'antd';
+import { Avatar, Descriptions } from 'antd';
 import { updateObject } from '../../store/utility';
 
 const UserDescription = props => {
@@ -17,16 +17,16 @@ const UserDescription = props => {
   return (
     <div>
       <Descriptions title="" layout="vertical">
-        <Descriptions.Item><img src = {userInfo.avatar} width="250" height="200" alt="avatar"/></Descriptions.Item>
+        <Descriptions.Item>{userInfo.avatar !== null ? <Avatar size={128} src={userInfo.avatar} /> : <Avatar size={128} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}</Descriptions.Item>
       </Descriptions>
       <Descriptions title="User info" layout="vertical">
         <Descriptions.Item label="UserName">{userInfo.username}</Descriptions.Item>
-        <Descriptions.Item label="Telephone">{userInfo.telephoneNumber !== "" ? userInfo.telephoneNumber : `undefined`}</Descriptions.Item>
-        <Descriptions.Item label="Live in">{userInfo.homeCountry !== "" ? userInfo.homeCountry : `undefined`}</Descriptions.Item>
+        <Descriptions.Item label="Telephone">{userInfo.telephoneNumber !== null && userInfo.telephoneNumber !== "" ? userInfo.telephoneNumber : `undefined`}</Descriptions.Item>
+        <Descriptions.Item label="Live in">{userInfo.homeCountry !== null && userInfo.homeCountry !== "" ? userInfo.homeCountry : `undefined`}</Descriptions.Item>
         <Descriptions.Item label="Address" span={2}>
-          {userInfo.address !== "" ? userInfo.address : `undefined`}
+          {userInfo.address !== null && userInfo.address !== ""  ? userInfo.address : `undefined`}
         </Descriptions.Item>
-        <Descriptions.Item label="About me">{userInfo.desctiption !== "" ? userInfo.desctiption : `undefined`}</Descriptions.Item>
+        <Descriptions.Item label="About me">{userInfo.desctiption !== null && userInfo.desctiption !== "" ? userInfo.desctiption : `undefined`}</Descriptions.Item>
       </Descriptions>
     </div>
 
