cd1095c8b6e7c4138ea48d1a391d52db4a1b4594
root <n.osypa666@gmail.com>
2020-07-12 19:16:23 +0000

    created ability to send friend requests

--- frontend/src/components/UserDescription/UserDescription.js
+++ frontend/src/components/UserDescription/UserDescription.js
@@ -2,10 +2,34 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Avatar, Descriptions } from 'antd';
 import PopupForm from '../PopupForm/PopupForm';
-
+import Cookies from 'js-cookie'
 const UserDescription = props => {
 
   const [userInfo, updateUserInfo] = useState({})
+  let csrftoken = Cookies.get('csrftoken')
+  let config = {
+    headers: {
+      'X-CSRFToken': csrftoken,
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const sendFriendRequest = () => {
+    let getId = async () => {
+      let response = await axios.get('http://localhost:8000/api/users/' + localStorage.username + '/')
+      console.log(response)
+      return response.data.id
+    }
+    getId().then(myID => {
+      axios.post('http://localhost:8000/api/friendRequests/', {
+        'from_user': myID,
+        'to_user': userInfo.id
+      }, config)
+        .then(() => console.log("success"))
+        .catch(err => console.log(err.response))
+    }).catch(e => { console.log(e.response) })
+  }
+
 
   useEffect(() => {
     axios.get('http://localhost:8000/api/users/' + props.displayUser + '/')
@@ -25,16 +49,16 @@ const UserDescription = props => {
         <Descriptions.Item label="Telephone">{userInfo.telephoneNumber !== null && userInfo.telephoneNumber !== "" ? userInfo.telephoneNumber : `undefined`}</Descriptions.Item>
         <Descriptions.Item label="Live in">{userInfo.homeCountry !== null && userInfo.homeCountry !== "" ? userInfo.homeCountry : `undefined`}</Descriptions.Item>
         <Descriptions.Item label="Address" span={2}>
-          {userInfo.address !== null && userInfo.address !== ""  ? userInfo.address : `undefined`}
+          {userInfo.address !== null && userInfo.address !== "" ? userInfo.address : `undefined`}
         </Descriptions.Item>
         <Descriptions.Item label="About me">{userInfo.desctiption !== null && userInfo.desctiption !== "" ? userInfo.desctiption : `undefined`}</Descriptions.Item>
       </Descriptions>
       {
         props.displayUser === localStorage.username
-        ? <PopupForm placeholderValues = {userInfo}/>
-        : <button type="submit">Add friend</button>
+          ? <PopupForm placeholderValues={userInfo} />
+          : <button type="submit" onClick={sendFriendRequest}>Add friend</button>
       }
-      
+
     </div>
 
   );
