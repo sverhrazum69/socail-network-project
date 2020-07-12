35a97137bdfaeff702a8de864fa713edde23971d
root <n.osypa666@gmail.com>
2020-07-12 12:36:34 +0000

    fixed img bug

--- frontend/src/components/FriendList/FriendList.js
+++ frontend/src/components/FriendList/FriendList.js
@@ -7,32 +7,33 @@ import axios from 'axios'
 
 const FirendList = props => {
     const [imageLinks, updateLinks] = useState([])
-
+   
     useEffect(() => {
         axios.get("http://localhost:8000/api/users/" + props.displayUser + "/")
             .then(response => {
+            
                 response.data.friends.forEach(userID => {
+                    
                     axios.get("http://localhost:8000/api/users/?id=" + userID)   
                     .then(res => {
-                        updateLinks(imageLinks.push({id:userID,avatar:res.data[0].avatar}))
+                        updateLinks(imageLinks => [...imageLinks,{id:userID,avatar:res.data[0].avatar}])
+                        
                     }).catch(err => {console.log(err)}) 
                 });
-        
             }).catch(err => { console.log(err) })
-
-    })
-    console.log(imageLinks)
+    },[])
+    
     return (
         <div>
+            
             <h2>Friend List</h2>
             <div className={fm.wrapper}>
                 
-                {[imageLinks].map((value, index) => (
+                {imageLinks.map((value, index) => (
                     
                     value.avatar !== null
                         ? <a href="google.com"><Avatar src={value.avatar} size={128} key={index} /></a>
                         : <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" href="google.com" size={128} key={index} />
-        
                     
                 ))}
                 
