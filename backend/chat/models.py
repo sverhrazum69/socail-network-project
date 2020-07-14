5efd9051eed50a969e95330566a0b84a97dcac12
root <n.osypa666@gmail.com>
2020-07-14 12:13:01 +0000

    created message model

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Messages(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='author')
+    content = models.CharField(max_length=30)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def get_last_10_messages(self):
+        return Messages.objects.order_by('-date').all()[10]
